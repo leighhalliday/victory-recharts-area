3a527eef78253c1648aa05227acaea17b81976dd
Leigh Halliday <leighhalliday@gmail.com>
2019-12-11 15:52:47 -0500

    Final version of demo comparing victory and recharts area charts

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { VictoryChart, VictoryArea, VictoryTheme } from "victory";
+import {
+  AreaChart,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Area,
+  Tooltip,
+  ResponsiveContainer,
+  LabelList
+} from "recharts";
 
-function App() {
+const data = [
+  { x: "2015", y: 6 },
+  { x: "2016", y: 13 },
+  { x: "2017", y: 17 },
+  { x: "2018", y: 26 },
+  { x: "2019", y: 38 }
+];
+
+const colors = {
+  teal: "#009688",
+  blueGrey: "#607D8B",
+  lightGrey: "#eee"
+};
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Commits By Year</h1>
+
+      <h2>Victory</h2>
+      <div style={{ maxWidth: "800px", margin: "0 auto" }}>
+        <VictoryChart theme={VictoryTheme.material} width={800} height={400}>
+          <VictoryArea
+            data={data}
+            labels={({ datum }) => datum.y}
+            style={{ data: { fill: colors.teal } }}
+          />
+        </VictoryChart>
+      </div>
+
+      <h2>Recharts</h2>
+      <div style={{ maxWidth: "800px", margin: "0 auto", height: "400px" }}>
+        <ResponsiveContainer>
+          <AreaChart
+            data={data}
+            margin={{ top: 25, right: 25, bottom: 25, left: 0 }}
+          >
+            <XAxis dataKey="x" />
+            <YAxis dataKey="y" />
+            <Area
+              dataKey="y"
+              isAnimationActive={false}
+              name="Repos"
+              fill={colors.teal}
+              stroke={colors.blueGrey}
+            >
+              <LabelList dataKey="y" position="top" offset={10} />
+            </Area>
+            <CartesianGrid stroke={colors.lightGrey} strokeDasharray="5 5" />
+            <Tooltip />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
+    </>
   );
 }
-
-export default App;
---
+++ src/data.js
@@ -0,0 +1,436 @@
+const raw = {
+  data: {
+    viewer: {
+      repositories: {
+        nodes: [
+          {
+            createdAt: "2015-11-05T12:37:01Z",
+            name: "chat-app-demo"
+          },
+          {
+            createdAt: "2015-11-24T15:21:26Z",
+            name: "assets"
+          },
+          {
+            createdAt: "2015-11-24T15:22:03Z",
+            name: "helpers"
+          },
+          {
+            createdAt: "2015-11-27T19:55:52Z",
+            name: "soccer_app"
+          },
+          {
+            createdAt: "2015-11-30T23:34:48Z",
+            name: "lotus"
+          },
+          {
+            createdAt: "2015-12-13T18:37:20Z",
+            name: "form-dump"
+          },
+          {
+            createdAt: "2016-01-27T20:28:47Z",
+            name: "gli_demo"
+          },
+          {
+            createdAt: "2016-02-06T16:10:28Z",
+            name: "twitter_rails"
+          },
+          {
+            createdAt: "2016-02-06T16:11:33Z",
+            name: "twitter_phoenix"
+          },
+          {
+            createdAt: "2016-03-26T23:08:22Z",
+            name: "rspec_api_documentation"
+          },
+          {
+            createdAt: "2016-04-16T21:34:53Z",
+            name: "phptest"
+          },
+          {
+            createdAt: "2016-05-01T11:53:10Z",
+            name: "padder"
+          },
+          {
+            createdAt: "2016-06-13T22:11:29Z",
+            name: "slate"
+          },
+          {
+            createdAt: "2016-06-15T18:00:07Z",
+            name: "plaid-ruby"
+          },
+          {
+            createdAt: "2016-07-19T13:16:58Z",
+            name: "blog"
+          },
+          {
+            createdAt: "2016-09-20T02:55:05Z",
+            name: "alpaca_llama_monolith"
+          },
+          {
+            createdAt: "2016-09-20T03:09:40Z",
+            name: "alpaca_llama_purchase"
+          },
+          {
+            createdAt: "2016-09-20T03:10:12Z",
+            name: "alpaca_llama_shipping"
+          },
+          {
+            createdAt: "2016-10-12T01:50:35Z",
+            name: "barcoder"
+          },
+          {
+            createdAt: "2017-01-17T02:56:35Z",
+            name: "pescadito-react-training"
+          },
+          {
+            createdAt: "2017-03-31T19:00:29Z",
+            name: "strong_locals"
+          },
+          {
+            createdAt: "2017-04-09T18:13:27Z",
+            name: "endpoints-practice"
+          },
+          {
+            createdAt: "2017-05-01T12:05:46Z",
+            name: "home-demo"
+          },
+          {
+            createdAt: "2017-05-29T19:58:10Z",
+            name: "webpack-starter"
+          },
+          {
+            createdAt: "2017-06-27T11:07:06Z",
+            name: "teamsnap_rb"
+          },
+          {
+            createdAt: "2017-06-27T11:08:24Z",
+            name: "omniauth-teamsnap"
+          },
+          {
+            createdAt: "2017-06-27T22:17:06Z",
+            name: "mobx-demo"
+          },
+          {
+            createdAt: "2017-07-10T02:24:35Z",
+            name: "tag-along"
+          },
+          {
+            createdAt: "2017-07-19T23:40:57Z",
+            name: "sharebibles"
+          },
+          {
+            createdAt: "2017-08-07T21:19:31Z",
+            name: "landbnb"
+          },
+          {
+            createdAt: "2017-09-11T19:54:58Z",
+            name: "test-gi"
+          },
+          {
+            createdAt: "2017-09-26T06:41:24Z",
+            name: "tda-wordpress-2017"
+          },
+          {
+            createdAt: "2017-09-28T14:33:37Z",
+            name: "geo-viz"
+          },
+          {
+            createdAt: "2017-10-24T02:12:48Z",
+            name: "create-react-app-mobx"
+          },
+          {
+            createdAt: "2017-11-11T15:40:21Z",
+            name: "animalitos-vanilla"
+          },
+          {
+            createdAt: "2017-12-24T18:11:00Z",
+            name: "stellarterm"
+          },
+          {
+            createdAt: "2018-01-26T03:27:06Z",
+            name: "sharebibles-website"
+          },
+          {
+            createdAt: "2018-02-09T15:17:31Z",
+            name: "mobx-decorators-without-ejecting"
+          },
+          {
+            createdAt: "2018-02-11T15:15:34Z",
+            name: "demo-node-app"
+          },
+          {
+            createdAt: "2018-02-16T02:36:45Z",
+            name: "sharebibles-admin"
+          },
+          {
+            createdAt: "2018-02-17T14:41:27Z",
+            name: "invoice-mobx-state-tree"
+          },
+          {
+            createdAt: "2018-03-07T01:53:44Z",
+            name: "mobx-async-actions"
+          },
+          {
+            createdAt: "2018-03-17T01:11:57Z",
+            name: "the-clima-front"
+          },
+          {
+            createdAt: "2018-03-24T20:13:08Z",
+            name: "easy-mobx-redux-comparison"
+          },
+          {
+            createdAt: "2018-03-28T16:04:00Z",
+            name: "feed-challenge-api"
+          },
+          {
+            createdAt: "2018-03-28T16:13:04Z",
+            name: "feed-challenge-rails"
+          },
+          {
+            createdAt: "2018-03-28T16:14:22Z",
+            name: "feed-challenge-react"
+          },
+          {
+            createdAt: "2018-04-02T19:43:51Z",
+            name: "demo-error-boundary"
+          },
+          {
+            createdAt: "2018-04-10T20:31:34Z",
+            name: "apollo-client"
+          },
+          {
+            createdAt: "2018-04-25T10:44:41Z",
+            name: "graphql-git"
+          },
+          {
+            createdAt: "2018-09-18T19:08:44Z",
+            name: "fullstackaudit"
+          },
+          {
+            createdAt: "2018-09-21T21:37:10Z",
+            name: "fsa-audit-generator"
+          },
+          {
+            createdAt: "2018-10-13T20:43:58Z",
+            name: "github-stars-graphql"
+          },
+          {
+            createdAt: "2018-10-20T16:16:02Z",
+            name: "githubstatus"
+          },
+          {
+            createdAt: "2018-10-25T16:58:01Z",
+            name: "reactjs.org"
+          },
+          {
+            createdAt: "2018-11-08T21:36:02Z",
+            name: "strong_migrations"
+          },
+          {
+            createdAt: "2018-12-01T03:17:44Z",
+            name: "TypeScript"
+          },
+          {
+            createdAt: "2018-12-01T03:22:16Z",
+            name: "TypeScript-Handbook"
+          },
+          {
+            createdAt: "2018-12-01T19:05:29Z",
+            name: "gatsby-typescript"
+          },
+          {
+            createdAt: "2018-12-05T12:11:44Z",
+            name: "react-with-typescript"
+          },
+          {
+            createdAt: "2018-12-09T22:02:24Z",
+            name: "react-ref-demo"
+          },
+          {
+            createdAt: "2018-12-21T16:14:20Z",
+            name: "immer-demo"
+          },
+          {
+            createdAt: "2019-01-04T17:20:48Z",
+            name: "callbacks-to-promises"
+          },
+          {
+            createdAt: "2019-01-20T15:06:29Z",
+            name: "create-react-app"
+          },
+          {
+            createdAt: "2019-01-20T20:43:59Z",
+            name: "react-suspense-demo"
+          },
+          {
+            createdAt: "2019-01-21T20:25:51Z",
+            name: "dad-jokes"
+          },
+          {
+            createdAt: "2019-02-10T01:01:11Z",
+            name: "phased-web"
+          },
+          {
+            createdAt: "2019-02-10T01:03:06Z",
+            name: "phased-api"
+          },
+          {
+            createdAt: "2019-02-13T22:18:34Z",
+            name: "learning-react-testing-library"
+          },
+          {
+            createdAt: "2019-02-14T18:58:19Z",
+            name: "use-effect-example"
+          },
+          {
+            createdAt: "2019-02-25T00:03:31Z",
+            name: "demo-mobx-espanol"
+          },
+          {
+            createdAt: "2019-03-07T00:14:17Z",
+            name: "leigh-notes-api"
+          },
+          {
+            createdAt: "2019-03-12T14:12:56Z",
+            name: "awesome-personal-blogs"
+          },
+          {
+            createdAt: "2019-03-12T14:32:22Z",
+            name: "clima-test"
+          },
+          {
+            createdAt: "2019-03-13T18:39:28Z",
+            name: "leigh-notes-web"
+          },
+          {
+            createdAt: "2019-03-21T19:48:52Z",
+            name: "apollo-tooling"
+          },
+          {
+            createdAt: "2019-03-24T23:45:59Z",
+            name: "apollo-generating-types"
+          },
+          {
+            createdAt: "2019-04-13T17:37:36Z",
+            name: "mapbox-react-demo"
+          },
+          {
+            createdAt: "2019-05-02T14:51:17Z",
+            name: "google-maps-react-demo"
+          },
+          {
+            createdAt: "2019-07-07T22:16:36Z",
+            name: "asts-markdown-mdx"
+          },
+          {
+            createdAt: "2019-07-08T19:58:31Z",
+            name: "og-image"
+          },
+          {
+            createdAt: "2019-07-11T15:47:12Z",
+            name: "schwans-landing"
+          },
+          {
+            createdAt: "2019-07-13T21:04:41Z",
+            name: "generate-og-image"
+          },
+          {
+            createdAt: "2019-08-05T22:22:12Z",
+            name: "react-forms-demo"
+          },
+          {
+            createdAt: "2019-08-18T22:30:39Z",
+            name: "graphql-batch-example"
+          },
+          {
+            createdAt: "2019-08-31T18:12:03Z",
+            name: "fgc"
+          },
+          {
+            createdAt: "2019-09-01T20:08:24Z",
+            name: "uses.tools"
+          },
+          {
+            createdAt: "2019-09-02T23:53:45Z",
+            name: "video-state-machine"
+          },
+          {
+            createdAt: "2019-09-05T16:00:09Z",
+            name: "nextjs-routing-test"
+          },
+          {
+            createdAt: "2019-09-07T14:56:35Z",
+            name: "secrets-env-vars-nextjs-now"
+          },
+          {
+            createdAt: "2019-09-07T16:59:05Z",
+            name: "demo-google-places-react"
+          },
+          {
+            createdAt: "2019-09-11T14:52:15Z",
+            name: "demo-infinite-scroll"
+          },
+          {
+            createdAt: "2019-10-06T18:01:51Z",
+            name: "intersection-observer-demo"
+          },
+          {
+            createdAt: "2019-10-06T19:57:04Z",
+            name: "graphql-react-demo"
+          },
+          {
+            createdAt: "2019-10-08T00:27:58Z",
+            name: "flipgive_challenge"
+          },
+          {
+            createdAt: "2019-10-10T16:27:31Z",
+            name: "flipgive-challenge"
+          },
+          {
+            createdAt: "2019-10-19T21:19:05Z",
+            name: "materialized-views-demo"
+          },
+          {
+            createdAt: "2019-10-28T15:51:30Z",
+            name: "magical_api_reader"
+          },
+          {
+            createdAt: "2019-11-15T14:44:19Z",
+            name: "redux-context-reducers"
+          },
+          {
+            createdAt: "2019-11-30T22:26:28Z",
+            name: "mobx2020"
+          }
+        ]
+      }
+    }
+  }
+};
+
+// extract year from each repo createdAt
+const repoYears = raw.data.viewer.repositories.nodes.map(repo =>
+  parseInt(repo.createdAt.substr(0, 4), 10)
+);
+
+// build object with each year initialized to 0
+const years = {};
+for (let i = Math.min(...repoYears); i <= Math.max(...repoYears); i++) {
+  years[i] = 0;
+}
+
+// populate years object with actual counts
+const countsByYearObject = repoYears.reduce(
+  (acc, year) => {
+    acc[year] += 1;
+    return acc;
+  },
+  { ...years }
+);
+
+// convert object into array format victory wants
+export const countsByYear = Object.keys(countsByYearObject).map(year => ({
+  x: year,
+  y: countsByYearObject[year]
+}));
